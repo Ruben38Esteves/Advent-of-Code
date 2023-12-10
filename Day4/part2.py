5e3c6080a8e4ff55a69af06ea7532b6e6834e714
Ruben Esteves <92651281+Ruben38Esteves@users.noreply.github.com>
2023-12-10 23:52:01 +0000

    tidying the code

--- Day4/part2.py
+++ Day4/part2.py
@@ -1,16 +1,14 @@
-i = 0
-copies= {}
-sum=0
 with open('Day4/input.txt') as file:
+    i = 0
+    copies= {}
+    sum=0
     for line in file:
         i+=1
-        copies[i] = copies.get(i, 0) +1
+        copies[i] = copies.get(i, 0) + 1
         line = line.strip()
         line = line.split('|')
-        mynums = line[0].split(':')[1].split(' ')
-        mynums = [item for item in mynums if item != '']
-        gamenums = line[1].split(' ')
-        count = len([item for item in gamenums if item != '' and item in mynums])
+        mynums = [item for item in line[0].split(':')[1].split(' ') if item != '']
+        count = len([item for item in line[1].split(' ') if item != '' and item in mynums])
         while copies[i] > 0:
             for x in range(i+1, i+count+1):
                 copies[x] = copies.get(x, 0) + 1
